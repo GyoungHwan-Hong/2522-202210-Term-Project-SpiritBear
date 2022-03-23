ff7bedcbe9c1e39617090549080caf53d653a67f
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-03-22 19:24:14 -0700

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ca.bcit.comp2522.termproject.spiritbear {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ca.bcit.comp2522.termproject.spiritbear to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.spiritbear;
+}
