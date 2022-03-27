bf8e7494e8c658edf4f453ad4019e35539e7803a
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-03-26 17:20:22 -0700

    Add bears images and tests moving many image views.

--- src/main/java/ca/bcit/comp2522/termproject/spiritbear/BearMove.java
+++ src/main/java/ca/bcit/comp2522/termproject/spiritbear/BearMove.java
@@ -13,19 +13,48 @@ public class BearMove extends Application {
 
     public static final int JUMP = 10;
 
-    private ImageView imageView;
+    private ImageView BearView;
+    private ImageView FireBearView;
+    private ImageView WaterBearView;
+    private ImageView EarthBearView;
+    private ImageView SpiritBearView;
+
 
     @Override
     public void start(Stage stage) {
 
-        Image bear = new Image("Bear.png", true);
-        imageView = new ImageView(bear);
+        Image bear = new Image("NormalBear.png", true);
+        Image fireBear = new Image("FireBear.png", true);
+        Image waterBear = new Image("FireWaterBear.png", true);
+        Image earthBear = new Image("FireWaterEarthBear.png", true);
+        Image spiritBear = new Image("SpiritBear.png", true);
+
+        BearView = new ImageView(bear);
+        FireBearView = new ImageView(fireBear);
+        WaterBearView = new ImageView(waterBear);
+        EarthBearView = new ImageView(earthBear);
+        SpiritBearView = new ImageView(spiritBear);
+
 
         final int bearStartCoordinate = 300;
-        imageView.setX(bearStartCoordinate);
-        imageView.setY(bearStartCoordinate);
+        BearView.setX(bearStartCoordinate + 80);
+        BearView.setY(bearStartCoordinate);
 
-        Group root = new Group(imageView);
+        FireBearView.setX(bearStartCoordinate + 40);
+        FireBearView.setY(bearStartCoordinate);
+
+        WaterBearView.setX(bearStartCoordinate);
+        WaterBearView.setY(bearStartCoordinate);
+
+        EarthBearView.setX(bearStartCoordinate - 40);
+        EarthBearView.setY(bearStartCoordinate);
+
+        SpiritBearView.setX(bearStartCoordinate - 80);
+        SpiritBearView.setY(bearStartCoordinate);
+
+        Group root = new Group();
+
+        root.getChildren().addAll(BearView,FireBearView,WaterBearView,EarthBearView,SpiritBearView);
 
         final int appHeight = 600;
         final int appWidth = 600;
@@ -42,15 +71,37 @@ public class BearMove extends Application {
 
     private void processKeyPress(KeyEvent keyEvent) {
         switch (keyEvent.getCode()) {
-            case UP -> imageView.setY(imageView.getY() - JUMP);
-            case DOWN -> imageView.setY(imageView.getY() + JUMP);
-            case LEFT -> imageView.setX(imageView.getX() - JUMP);
-            case RIGHT -> imageView.setX(imageView.getX() + JUMP);
-            default -> {
-            } // Does nothing if it's not an arrow key.
-
+            case UP:
+                BearView.setY(BearView.getY() - JUMP);
+                FireBearView.setY(FireBearView.getY() - JUMP);
+                WaterBearView.setY(WaterBearView.getY() - JUMP);
+                EarthBearView.setY(EarthBearView.getY() - JUMP);
+                SpiritBearView.setY(SpiritBearView.getY() - JUMP);
+                break;
+            case DOWN:
+                BearView.setY(BearView.getY() + JUMP);
+                FireBearView.setY(FireBearView.getY() + JUMP);
+                WaterBearView.setY(WaterBearView.getY() + JUMP);
+                EarthBearView.setY(EarthBearView.getY() + JUMP);
+                SpiritBearView.setY(SpiritBearView.getY() + JUMP);
+                break;
+            case LEFT:
+                BearView.setX(BearView.getX() - JUMP);
+                FireBearView.setX(FireBearView.getX() - JUMP);
+                WaterBearView.setX(WaterBearView.getX() - JUMP);
+                EarthBearView.setX(EarthBearView.getX() - JUMP);
+                SpiritBearView.setX(SpiritBearView.getX() - JUMP);
+                break;
+            case RIGHT:
+                BearView.setX(BearView.getX() + JUMP);
+                FireBearView.setX(FireBearView.getX() + JUMP);
+                WaterBearView.setX(WaterBearView.getX() + JUMP);
+                EarthBearView.setX(EarthBearView.getX() + JUMP);
+                SpiritBearView.setX(SpiritBearView.getX() + JUMP);
+                break;
+            default:
+                break; // Does nothing if it's not an arrow key.
         }
     }
-
     public static void main(String[] args) { Application.launch(args);}
 }
