b5bea3540c1a90cd01e0c728abe60c4943daa4ab
David Lee <dvd.y.lee@gmail.com>
2022-04-04 14:27:23 -0700

    Added Images. created NPC and Stage2 quiz.

---
+++ src/main/java/ca/bcit/comp2522/termproject/spiritbear/quizNPC.java
@@ -0,0 +1,48 @@
+package ca.bcit.comp2522.termproject.spiritbear;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class quizNPC extends Obstacle{
+    Image npc;
+    ImageView npcImageView;
+    int npcId;
+
+    public quizNPC(int xCoordinate, int yCoordinate) {
+        super(xCoordinate, yCoordinate, 40, 40);
+        this.npc = new Image("totem_pole.png", true);
+        this.npcImageView = new ImageView(npc);
+        this.npcId = 3;
+    }
+
+
+    public void initPosition(int[][] arr) {
+
+        int si = xCoordinate - height / 2;
+        int fi = xCoordinate + height / 2;
+        int sj = yCoordinate - width / 2;
+        int fj = yCoordinate + width / 2;
+
+        this.npcImageView.setX(this.xCoordinate);
+        this.npcImageView.setY(this.yCoordinate);
+
+        for ( int i = si; i <= fi; i++) {
+            for (int j = sj; j <= fj ; j++) {
+                arr[i][j] = npcId;
+            }
+        }
+    }
+
+    public int getNpcId(final int inputNPCId) {
+        if (inputNPCId == this.npcId) {
+            return this.npcId;
+        }
+        return 0;
+    }
+
+
+
+    public int setPosition() {
+        return 1;
+    }
+}
