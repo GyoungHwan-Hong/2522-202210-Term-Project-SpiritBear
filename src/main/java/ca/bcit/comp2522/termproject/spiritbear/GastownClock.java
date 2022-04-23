2f851e90573aef0cb318b52f5a6e05897003bd31
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-04-22 18:43:03 -0700

    Add more obstacles in stage4. And, Change function which is to initiate position logic.

--- src/main/java/ca/bcit/comp2522/termproject/spiritbear/GastownClock.java
+++ src/main/java/ca/bcit/comp2522/termproject/spiritbear/GastownClock.java
@@ -3,13 +3,13 @@ package ca.bcit.comp2522.termproject.spiritbear;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class gastownClock extends Obstacle{
+public class GastownClock extends Obstacle{
 
     Image clockImage;
     ImageView clockImageView;
 
-    public gastownClock(int xCoordinate, int yCoordinate) {
-        super(xCoordinate, yCoordinate, 60, 50, 3);
+    public GastownClock(int xCoordinate, int yCoordinate) {
+        super(xCoordinate, yCoordinate, 80, 43, 3);
         this.clockImage = new Image("gastownclock.png", true);
         this.clockImageView = new ImageView(clockImage);
 
